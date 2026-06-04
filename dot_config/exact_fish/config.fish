5589963bee921a8ea7e4459652a19ea48b568829
Samuel Gabel <samuel.gabel@idealo.de>
2026-06-04 11:38:56 +0200

    change fish includes

--- dot_config/exact_fish/config.fish
+++ dot_config/exact_fish/config.fish
@@ -5,7 +5,6 @@ end
 
 set -xg AWS_REGION eu-central-1
 set -xg ANTHROPIC_SMALL_FAST_MODEL 'arn:aws:bedrock:eu-central-1:801348203568:inference-profile/eu.anthropic.claude-haiku-4-5-20251001-v1:0'
-#set -xg ANTHROPIC_MODEL 'arn:aws:bedrock:eu-central-1:801348203568:inference-profile/eu.anthropic.claude-3-7-sonnet-20250219-v1:0'
 set -xg ANTHROPIC_MODEL 'arn:aws:bedrock:eu-central-1:801348203568:application-inference-profile/4q38bqx2lok4'
 set -xg CLAUDE_CODE_DISABLE_EXPERIMENTAL_BETAS 1
 set -xg CLAUDE_CODE_USE_BEDROCK 1
